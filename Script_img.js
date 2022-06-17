0294acc5cef1c57e0cc45e9efe1ba08c79241f71
Adnan Korchi <k.adnen23@hotmail.com>
2022-06-17 23:28:49 +0100

    Modification du lien vers les images

--- Script_img.js
+++ Script_img.js
@@ -3,7 +3,7 @@ export function ajouterImageAnnivSale() {
     for (let index = 2; index < 12; index++) {
         var container_img_anniv = document.querySelector("#img_anniversaire")
         var imgAnniversaire = ` <div class="carousel-item">
-                                <img src="/G_Anniversaire/PhotoAnniv${index}.jpg"
+                                <img src="G_Anniversaire/PhotoAnniv${index}.jpg"
                                     class="img-thumbnail  border border-4 mt-2 w-75 h-75 shadow-lg"
                                     style="max-height: 80%;" alt="Gateau d'anniversaire${index}">
                                 </div>`
@@ -13,7 +13,7 @@ export function ajouterImageAnnivSale() {
     for (let index = 2; index < 4; index++) {
         var container_img_sale = document.querySelector("#img_sale")
         var imgSales = `<div class="carousel-item">
-                        <img src="/G_Salés/PhotoSale${index}.jpg"
+                        <img src="G_Salés/PhotoSale${index}.jpg"
                             class="img-thumbnail  border border-4 mt-2 w-75 h-75 shadow-lg" alt="salés${index}">
                         </div>`
         container_img_sale.innerHTML += imgSales
